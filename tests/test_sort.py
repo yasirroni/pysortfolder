1d10efd57dc529e64ffd4dfb1dfe88790db2c349
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-06-03 15:35:13 +0700

    update test

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -5,11 +5,11 @@ import os
 
 class TestSort:
     def test_sort_folders(self):
-        print(SortFolders(dir_path="tests/data", reverse=True).sort_by_size)
+        print(SortFolders(dir_path="tests/data").sort_by_size())
 
     def test_sort_folders_and_files(self):
-        print(SortFoldersAndFiles(dir_path="tests/data", reverse=True).sort_by_size)
+        print(SortFoldersAndFiles(dir_path="tests/data").sort_by_size())
 
     def test_check_current_folder_size(self):
         sf = SortFolders(dir_path="tests/data")
-        sf.dir_size
+        sf.dir_size()
