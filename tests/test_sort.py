01f0f0592ab37714789d60a78835b265e62537a2
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-12-13 21:38:39 +0700

    refactor pre-commit; refactor test without class

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -4,26 +4,29 @@ from pysortfolder import PySortFolder, dir_size
 path = "tests/data"
 
 
-class TestSort:
-    """Test using pytest
-    pytest --lf -rA -c pyproject.toml --cov-report term --cov=pysortfolder tests/
-    """
+"""Test using pytest
+pytest --lf -rA -c pyproject.toml --cov-report term --cov=pysortfolder tests/
+"""
 
-    def test_get_tree(self):
-        sf = PySortFolder(path=path)
-        tree = sf.get_tree()
-        assert isinstance(sf._tree, dict)
-        assert isinstance(tree, dict)
 
-    def test_print_tree(self):
-        PySortFolder(path=path).print_tree()
+def test_get_tree():
+    sf = PySortFolder(path=path)
+    tree = sf.get_tree()
+    assert isinstance(sf._tree, dict)
+    assert isinstance(tree, dict)
 
-    def test_check_current_folder_size(self):
-        size = dir_size(path=path)
-        # TODO: os based size
-        # assert size == 82 # ubuntu
-        # assert size == 87 # ubuntu
-        assert isinstance(size, int)
 
-    def test_version(self):
-        assert isinstance(pysortfolder.__version__, str)
+def test_print_tree():
+    PySortFolder(path=path).print_tree()
+
+
+def test_check_current_folder_size():
+    size = dir_size(path=path)
+    # TODO: os based size
+    # assert size == 82 # ubuntu
+    # assert size == 87 # ubuntu
+    assert isinstance(size, int)
+
+
+def test_version():
+    assert isinstance(pysortfolder.__version__, str)
